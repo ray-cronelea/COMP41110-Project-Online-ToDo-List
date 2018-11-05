90689ec89d99621b0d1c6f7df108694142581f43
Ray <ray@cronelea.ie>
2018-11-05 14:06:09 +0000

    Barebones spring boot app added

---
+++ src/test/java/ie/raywilson/todo/CloudTodo/CloudTodoApplicationTests.java
@@ -0,0 +1,16 @@
+package ie.raywilson.todo.CloudTodo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudTodoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
